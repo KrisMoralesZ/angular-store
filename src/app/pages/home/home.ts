966e8bd144c0e814c4f9982466b0c7e67c75ba47
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-07-21 10:08:04 -0700

    create header component

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Product } from '../../components/product/product';
+import { Header } from '../../components/header/header';
 
 @Component({
   selector: 'app-home',
-  imports: [Product],
+  imports: [Product, Header],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
