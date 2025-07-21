d423ee495a00a6cb939b8b19a4e5ccac320e7320
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-07-21 10:37:09 -0700

    refactor header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { Header } from './components/header/header';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: '<router-outlet/>',
+  imports:[Header, RouterOutlet],
+  templateUrl: './app.html',
 })
 export class App {
   protected title = 'store';
