065aa92504737ed96a92fe81b9a5362a1139ffc1
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-07-21 12:27:34 -0700

    add checkout and services pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { About } from './pages/about/about';
+import { Services } from './pages/services/services';
+import { Checkout } from './pages/checkout/checkout';
 
 export const routes: Routes = [
   {
@@ -8,7 +10,15 @@ export const routes: Routes = [
     component: Home,
   },
   {
-    path: 'about', 
-    component: About
+    path: 'about',
+    component: About,
+  },
+  {
+    path: 'services',
+    component: Services,
+  },
+  {
+    path: 'checkout',
+    component: Checkout,
   },
 ];
