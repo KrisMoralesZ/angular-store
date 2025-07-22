1004861efa60eb8bac24d7fff0bb85f2d086d76d
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-07-22 09:46:08 -0700

    add sideMenu

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, signal } from '@angular/core';
+import { IProduct } from '../../shared/models/product.model';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.html',
-  styleUrl: './header.css'
+  styleUrl: './header.css',
 })
 export class Header {
+  hideSideMenu = signal(true);
 
+  @Input({ required: true }) cart: IProduct[] = [];
+  total = signal(0);
+
+  toggleSideMenu() {
+    this.hideSideMenu.update((prevState) => !prevState);
+  }
 }
