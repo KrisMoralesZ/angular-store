dcc6d61788bbbd853df21c0c1105b88b1e3e7c2d
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-07-24 12:53:18 -0700

    show products from api

---
+++ src/app/shared/services/product-service.ts
@@ -0,0 +1,16 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IProduct } from '../models/product.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  private http = inject(HttpClient);
+
+  getProducts() {
+    return this.http.get<IProduct[]>(
+      'https://api.escuelajs.co/api/v1/products'
+    );
+  }
+}
