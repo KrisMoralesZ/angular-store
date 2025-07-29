1d9c2a2c4a3db6ce5c32d6a918ea0f85b80b32f5
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-07-29 12:36:55 -0700

    add categories service

--- src/app/pages/categories/categories.ts
+++ src/app/pages/categories/categories.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ICategory } from '@shared/models/category.model';
+import { CategoriesService } from '@shared/services/categories-service';
 
 @Component({
   selector: 'app-categories',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './categories.html',
-  styleUrl: './categories.css'
+  styleUrl: './categories.css',
 })
 export class Categories {
+  categories = signal<ICategory[]>([]);
 
+  private categoriesService = inject(CategoriesService);
+
+  ngOnInit() {
+    this.categoriesService.getAllCategories().subscribe({
+      next: (categories) => {
+        this.categories.set(categories);
+      },
+      error: () => {
+        console.error();
+      },
+    });
+  }
 }
