519c98d532f3a32b8b741fc1a24994d5b80c310c
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2025-08-15 11:56:06 -0700

    ssr integration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,14 +4,19 @@ import {
   provideZoneChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { routes } from './app.routes';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withFetch()),
+    provideClientHydration(withEventReplay()),
   ],
 };
